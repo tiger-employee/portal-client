821fb430219d40a5d3eda6718a78b351aca79c6f
Rob <robrichardsdpt@gmail.com>
2021-01-07 20:22:43 -0500

    emoji picker and chat function is working

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputEmoji from 'react-input-emoji'
+import './home.scss'
 
-const Home = () => {
+export default function Home () {
+  const [text, setText] = useState('')
+  const [message, setMessage] = useState([])
+
+  function handleOnEnter (text) {
+    setText(text)
+    setMessage(text)
+  }
+
   return (
-    <div>This is the home page.</div>
+    <div>
+      <div className='chat-content'>{message}</div>
+      <InputEmoji
+        value={text}
+        onChange={setText}
+        cleanOnEnter
+        onEnter={handleOnEnter}
+        placeholder='Type a message'
+      />
+    </div>
   )
 }
-
-export default Home
