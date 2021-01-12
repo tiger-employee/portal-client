90b99556378a5c6441d3268bd72747f7e2595b90
Rob <robrichardsdpt@gmail.com>
2021-01-11 23:01:34 -0500

    removed the home from always option, and fixed the route to reroute you to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ import './header.styles.scss'
 
 const authenticatedOptions = (
   <Fragment>
+    <Nav.Link href="#home">Home</Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
@@ -18,12 +19,6 @@ const unauthenticatedOptions = (
   </Fragment>
 )
 
-const alwaysOptions = (
-  <Fragment>
-    <Nav.Link to="/">Home</Nav.Link>
-  </Fragment>
-)
-
 const Header = ({ user }) => (
   <Navbar className='navbar' expand="md">
     <Navbar.Brand href="#">
@@ -33,7 +28,6 @@ const Header = ({ user }) => (
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
         { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-        { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
