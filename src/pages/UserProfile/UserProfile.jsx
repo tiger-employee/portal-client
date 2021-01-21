6f42cca7aa7de1da9d39be674ed0893203b1ce70
Rob <robrichardsdpt@gmail.com>
2021-01-21 17:56:56 -0500

    gives length of meditation array

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig.js'
 import Quote from '../../components/Quote/Quote'
 import BreakTimer from '../../components/BreakTimer/BreakTimer'
 import Meditation from '../../components/Meditation/Meditation'
 
 const Profile = ({ user }) => {
+  const [meditations, setMeditations] = useState([])
+
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}/meditations/`,
+      method: 'GET',
+      headers: {
+        Authorization: `Token token=${user.token}`
+      }
+    })
+      .then((res) => setMeditations(res.data.meditations))
+  }, []
+  )
+
   return (
-    <div>This is {user.email} profile page.
-      <Quote/>
-      <BreakTimer/>
-      <Meditation user={user}/>
+    <div className='profile-container'>{user.email} profile page.
+      You have meditated {meditations.length} times.
+    <Quote/>
+    <BreakTimer/>
+    <Meditation user={user}/>
     </div>
   )
 }
