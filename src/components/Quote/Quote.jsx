f37f4a3102e5c94e3b66efe19e4dfc1011cc6701
Rob <robrichardsdpt@gmail.com>
2021-01-21 10:55:15 -0500

    random gratitude quote generator working

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './quote.styles.scss'
+import quoteLibrary from './quoteLibrary'
 
 const Quote = () => {
+  const randomQuoteIndex = Math.floor(Math.random() * quoteLibrary.length)
   return (
-    <div className='quote-container'>This is the random quote</div>
+    <div className='quote-container'>{quoteLibrary[randomQuoteIndex]}</div>
   )
 }
 
