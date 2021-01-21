f041d022c1b5cd7435e1534ed34eedae423085d8
Rob <robrichardsdpt@gmail.com>
2021-01-21 11:02:43 -0500

    fixed typo

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Quote from '../../components/Quote/Quote'
 import BreakTimer from '../../components/BreakTimer/BreakTimer'
-import Mediation from '../../components/Meditation/Meditation'
+import Meditation from '../../components/Meditation/Meditation'
 
 const Profile = ({ user }) => {
   return (
     <div>This is {user.email} profile page.
       <Quote/>
       <BreakTimer/>
-      <Mediation/>
+      <Meditation/>
     </div>
   )
 }
