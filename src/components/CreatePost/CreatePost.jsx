cc20a82756d4719ac9abd64e3b484f710b261536
Rob <robrichardsdpt@gmail.com>
2021-02-02 10:35:40 -0500

    you can now use a dropdown to create a recommendation for a recipient.  Populating on their profile page.

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import apiUrl from '../../apiConfig.js'
 import EmojiTextArea from '../EmojiPicker/EmojiTextArea'
@@ -6,6 +6,7 @@ import './create-post.stylings.scss'
 
 const CreatePost = ({ user }) => {
   const [newPost, setNewPost] = useState({})
+  const [users, setUsers] = useState([])
   const [text, setText] = useState('')
   console.log(text)
   const handleContentChange = (text) => {
@@ -40,11 +41,27 @@ const CreatePost = ({ user }) => {
       .then(() => setNewPost({}))
   }
 
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}/users/`,
+      method: 'GET'
+    })
+      .then((res) => setUsers(res.data.users))
+  }, [])
+
+  const userOptionsJsx = users.map(userObj => {
+    if (userObj._id !== user._id) {
+      return <option key={userObj._id} id={userObj._id} value={userObj._id}>{`${userObj.firstName} ${userObj.lastName}`}</option>
+    }
+  })
   return (
     <div className='create-post-container'>
       Recognize someone <br/>
       <form id='create-post' onSubmit={handleSubmit}>
-        <input className='post-recipient' name="recipient" type="text" onChange={handleChange}></input> <br/>
+        <select name='recipient' defaultValue={'DEFAULT'} onChange={handleChange}>
+          <option value='DEFAULT' disabled hidden>Choose person to recognize!</option>
+          {userOptionsJsx}
+        </select>
         <EmojiTextArea
           setText= {setText}
           className="create-message-textarea"
